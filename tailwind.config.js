131b4a331877b0780e32607764c874448a1f20eb
Abi Thetchana <muthukumari.m28197@gmail.com>
2023-10-16 01:00:56 +0530

    title added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'main-grid': '2.9fr 1.1fr',
+        'second-grid': '2fr 1fr'
+      }
+    },
   },
   plugins: [],
 }
